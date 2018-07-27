3f1d7a5454aa14996e7f83dc6137167a75155087
thetigre <thetigre123@gmail.com>
2018-07-26 23:58:46 -0400

    commit hoy

--- cliente/index.js
+++ cliente/index.js
@@ -3,20 +3,22 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const path = require ('path')
-var mejor = "lo mejor esta por venir"
 
 // levantar el servidor http
 app.use(express.static(path.join(__dirname, 'pages')))
 app.get('/', function (req,res){
-  res.render('ejempo1.html')
+  res.render('index.html')
 })
 app.get('/ejemplo', function (req,res){
-  res.render('ejempo1.html')
+  res.render('ejemplo1.html')
 })
 app.get('/otro', function (req,res){
-  res.render('ejempo2.html')
+  res.render('ejemplo2.html')
 })
 
+app.get('/grafica', function (req,res){
+  res.render('graficas.html')
+})
 var server = http.createServer(app).listen(3000, function() {  
   console.log('Express server listening on port https://localhost:3000');
 });
